c4cb8e3b7db888c461768ad732cfa3e9989b3245
Abdullah <abdullahoguk@gmail.com>
2023-01-01 15:43:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-location.href = window.URLMap[location.pathname];
+LINKSSOURCE="https://raw.githubusercontent.com/abdullahoguk/assets/main/shortlinks.json"
+fetch(LINKSSOURCE)
+.then(function(res){return res.json()})
+.then(function(data){
+  URLMap=data
+  location.href = window.URLMap[location.pathname];
+})
+
